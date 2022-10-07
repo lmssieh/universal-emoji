be78cae2c78c19bdd0a37a361d6fda1c3c9fbecc
rust <53446488+lmssieh@users.noreply.github.com>
2022-10-07 23:25:31 +0100

    show notification when copying a kaomoji

--- kaomoji-website/components/Notification.tsx
+++ kaomoji-website/components/Notification.tsx
@@ -1,33 +1,29 @@
 // import { useRef, useState, useEffect } from 'react'
 // import {createPortal} from 'react-dom'
 
+import { useEffect } from "react";
+
 interface Props {
-  className?: string,
-  children?: React.ReactNode,
-  selector: string,
-  onClose: () => void
+	className?: string;
+	onClose: () => void;
 }
 
-function Notification({className, children, selector, onClose}: Props) {
-  return <div></div>;
-//   const ref = useRef()
-//   const [mounted, setMounted] = useState(false)
+function Notification({ className, onClose }: Props) {
+	useEffect(() => {
+		const timeout = setTimeout(onClose, 2000);
 
-//   useEffect(() => {
-//     ref.current = document.querySelector(selector)
-//     setMounted(true);
-//   });
-
-//   const html = <div
-//   onClick={() => onClose} 
-//   className='fixed cursor-pointer bottom-[1.5em] right-[1.5em] bg-green-100 text-green-800'>
-//     copied
-//     <button 
-//   onClick={() => onClose} 
-//   >clickme</button>
-//   </div>;
-
-//  return false ? createPortal(html, ref.current) : html
+		return () => clearTimeout(timeout);
+	});
+	return (
+		<div
+			onClick={onClose}
+			className={`${
+				className || " "
+			} fixed bottom-5 right-5 p-3  text-xl rounded font-poppins bg-green-100 text-green-500 cursor-pointer"`}
+		>
+			copied!
+		</div>
+	);
 }
 
-export default Notification
+export default Notification;
