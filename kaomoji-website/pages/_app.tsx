83d022d5b0ed7044f92fabac75dfa8e8e9da3e84
rust <53446488+lmssieh@users.noreply.github.com>
2022-10-07 20:43:36 +0100

    feat: add a custom font

--- kaomoji-website/pages/_app.tsx
+++ kaomoji-website/pages/_app.tsx
@@ -1,11 +1,14 @@
-import type { AppProps } from 'next/app'
-import 'windi.css'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import "windi.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className='relative '>
-     <Component {...pageProps} />
-     <div id="notification-portal"></div>
-  </div>
+	return (
+		<div className="relative ">
+			<Component {...pageProps} />
+			<div id="notification-portal"></div>
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
---
+++ kaomoji-website/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+function Document() {
+	return (
+		<Html>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
+
+export default Document;
